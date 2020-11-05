644ce49bdfe580a5bad9c1e40454dd15bd9a4264
roma038 <romalezgin1997@gmail.com>
2020-11-05 10:21:57 +0400

    Add project files.

---
+++ TutorialsTeacher/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TutorialsTeacher.Models;
+
+namespace TutorialsTeacher.Controllers
+{
+    public class HomeController : Controller
+    {
+        IList<Student> studentList = new List<Student>{
+                            new Student() { StudentId = 1, StudentName = "John", Age = 18, Gender = "Male" } ,
+                            new Student() { StudentId = 2, StudentName = "Sophiya", Age = 21, Gender = "Female" } ,
+                            new Student() { StudentId = 3, StudentName = "Bill", Age = 25, Gender = "Male"} ,
+                            new Student() { StudentId = 4, StudentName = "Ram" , Age = 20, Gender = "Male"} ,
+                            new Student() { StudentId = 5, StudentName = "Alice" , Age = 31 ,Gender = "Female"} ,
+                            new Student() { StudentId = 4, StudentName = "Christine" , Age = 17, Gender = "Female" } ,
+                            new Student() { StudentId = 4, StudentName = "Rob" , Age = 19, Gender = "Male" } };
+
+
+        public ActionResult Index()
+        {
+            
+            return View(studentList);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var std = studentList.Where(s => s.StudentId == id).FirstOrDefault();
+            return View(std);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student stud)
+        {
+            if (ModelState.IsValid)
+            {
+                var student = studentList.Where(s => s.StudentId == stud.StudentId).FirstOrDefault();
+                studentList.Remove(student);
+                studentList.Add(stud);
+                return RedirectToAction("Index");
+            }
+            return View(stud);
+        }
+
+    }
+}
---
+++ TutorialsTeacher/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace TutorialsTeacher.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        [Required(ErrorMessage ="The student name field is empty")]
+        public string StudentName { get; set; }
+        [Range(17,25)]
+        public int Age { get; set; }
+        [Required]
+        public string Gender { get; set; }
+    }
+}
